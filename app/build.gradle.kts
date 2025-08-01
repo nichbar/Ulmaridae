81e99b54a6123539e146d079b1107f58fe5cb71b
nichbar <cnichbar@gmail.com>
2025-08-01 11:08:41 +0800

    chore: update version to 0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "now.link.ulmaridae"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
