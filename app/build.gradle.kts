4a2d85752e0871c8794a71e608e391fab8bf49e6
nichbar <cnichbar@gmail.com>
2025-08-11 16:39:56 +0800

    chore: bump to v0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "now.link.ulmaridae"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
