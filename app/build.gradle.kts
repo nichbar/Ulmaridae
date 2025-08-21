3eeba9d92c881f2d061b464e42de39e48289f5f0
nichbar <cnichbar@gmail.com>
2025-08-21 11:37:14 +0800

    chore: bump to v0.6 (just to trigger a new build since komari-agent has new update)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "now.link.ulmaridae"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.5"
+        versionCode = 6
+        versionName = "0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
